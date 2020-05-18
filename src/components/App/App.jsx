aecd52c6d73fe2d45fbb6bceecd007c4610cb332
Josh Ford <thisisjoshford@gmail.com>
2020-05-17 18:15:19 -0700

    app cloned from restman

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import FormContainer from '../../containers/FormContainer/FormContainer';
+import Header from '../Header/Header';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header/>
+      <FormContainer/>
+    </>
+  );
 }
-  

45389b5f4cec75e408ac0d9319c72c03372e0c64
Josh Ford <thisisjoshford@gmail.com>
2020-05-17 18:35:16 -0700

    added context to provider & exported

--- src/hooks/Provider.jsx
+++ src/hooks/Provider.jsx
@@ -1,12 +1,20 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
 const RestyContext = createContext();
 
 export const RestyProvider = (children) => {
+  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
+  const [body, setBody] = useState('');
 
   return (
-    <RestyContext.Provider value ={}>
+    <RestyContext.Provider value ={{ url, method, body }}>
       {children}
     </RestyContext.Provider>
-  )
-}
+  );
+};
+
+export const useProvider = () => {
+  const context = useContext(RestyContext);
+  return context;
+};
